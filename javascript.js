3ad7f191105c8f73e79b4037d6a504b20447eb31
crisandryj <crisandryj@gmail.com>
2023-05-17 07:31:50 -0400

    slice each row from array

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
 const gameboard = (() => {
-  const board = [];
+  let board = [1,2,3,4,5,6,7,8,9];
+  //createboard
+  const createBoard = () => {
+    const firstRow = board.slice(0,3)
+    const secRow = board.slice(3,3)
+    const thirdRow = board.slice(0,3)
+    console.log({firstRow, secRow, thirdRow})
+  };
+  //render
+  createBoard()
   return {board}
-
 })();
 
 
-console.log(gameboard.board)
+const Player = (name) =>{
+};
