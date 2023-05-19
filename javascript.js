58e4bbe82c6f0aebbe76d166eff6522552646d62
crisandryj <crisandryj@gmail.com>
2023-05-19 14:39:23 -0400

    2d array

--- javascript.js
+++ javascript.js
@@ -2,15 +2,23 @@ const gameboard = (() => {
   const board = [];
   const rows = 3;
   const columns = 3;
-  //select
 
   //createboard
   const createBoard = () => {
+    for (let i = 0; i < rows.length; i++){
+      board[i] = []
+      for(let j = 0; j < columns.length;j++){
+      board[i].push(Cell())
+      }
+    }
+  };
+
+  function Cell(){
+    let value = 0
   };
 
   //render
-  createBoard()
-
+createBoard()
   return {board}
 })();
 
