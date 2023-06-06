b649301e048b1deab3a4adb1f05765cf1075fa7b
crisandryj <crisandryj@gmail.com>
2023-06-06 15:23:58 -0400

    Feat: Create columns and rows for board:

--- javascript.js
+++ javascript.js
@@ -3,7 +3,12 @@ const gameboard = (() => {
   const rows = 3;
   const columns = 3;
 
-  
+  for (i = 0; i < rows; i++){
+    board[i] = [];
+    for (c = 0; c < columns; c++){
+      board[i].push(Cell());
+    }
+  }
 
 })();
 
